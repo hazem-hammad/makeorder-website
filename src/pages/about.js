cfd52247926deae28a6c211eb6ba616113cf87b6
Amir <amir.labib@paynas.com>
2021-02-19 20:25:33 +0200

    modify project

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <h2>About Page here!</h2>;
+};
+
+export default About;
---
+++ src/pages/contact-us.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/header";
+
+const ContactUs = () => {
+  return (
+    <div>
+      <h1>Contact Us !!!</h1>
+      <Header />
+    </div>
+  );
+};
+
+export default ContactUs;
